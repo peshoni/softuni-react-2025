d9ea8eafd0b8cc4342ed0a69f42606326b2e242c
Petar Ivanov <petarivanovgap@gmail.com>
2025-11-06 18:58:46 +0200

    Material appBar with menus was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,69 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { useRegisterMutation, type UserFragment } from './../graphql/generated';
+import { ApolloError } from '@apollo/client';
+import ApplicationBar from './components/ApplicationBar';
 
-function App() { 
+function App() {
+  // const [isParentVisible, setIsParentVisible] = useState(true);
+  const [userAdded, setUser] = useState({} as UserFragment);
+  const [isServerOffline, setIsServerOffline] = useState(false);
+console.log(userAdded,isServerOffline)
+  // const { data, loading, error } = useLoginQuery({ variables: { email: 'pesho', password: 'pesho' } });
+  // console.log(data, loading, error);
+
+  const [mutate] = useRegisterMutation({
+    // variables: {
+    //   user: {
+    //     name: 'Pepe',
+    //     surname: 'Ivanov',
+    //     family: 'Ivanov',
+    //     gender: 'male',
+    //     email: 'aaa@aaa.bg',
+    //     password: 'aaaaa', 
+    //     role: 'customer' 
+    //   } 
+    // }
+  });
+
+  useEffect(() => {
+    console.log('MOUNT...');
+    mutate({
+      variables: {
+        user: {
+          name: 'Pepe',
+          surname: 'Ivanov',
+          family: 'Ivanov',
+          gender: 'male',
+          email: 'aaa@aaa.bg',
+          password: 'aaaaa',
+          role: 'customer'
+        }
+      }
+    }).then((res) => {
+      setUser(res.data?.insert_users_one as UserFragment);
+      console.log(res);
+    }).catch(e => {
+      if (e instanceof ApolloError) {
+        console.log(e);
+        setIsServerOffline(true);
+      }
+    });
+    // console.log(data, loading, error ); 
+  }, []);
 
   return (
     <>
-       hello world
+      <ApplicationBar />
+
+      {/* <h1>hello world</h1>
+      <button onClick={() => setIsParentVisible((curr) => !curr)}>toggle</button>
+      {isParentVisible
+        ? <Parent />
+        : <section>Disabled</section>
+      } */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { ApolloProvider } from '@apollo/client/react';
+import client from "./apolloClient.tsx";
+import { StyledEngineProvider } from '@mui/material/styles';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <ApolloProvider client={client}>
+    <StyledEngineProvider injectFirst> 
+      {/* <StrictMode> */}
+      <App />
+      {/* </StrictMode> */}
+    </StyledEngineProvider>
+  </ApolloProvider>
+);
